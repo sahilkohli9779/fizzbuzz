3b3c3136499727d58c4ee287195bf8238973b8c8
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 05:19:08 -0700

    Read problem statement and started with Task1

---
+++ task1.py
@@ -0,0 +1,7 @@
+target_file=input("Book1  Book2  Book3 ---- : ")
+if target_file=="Book1":
+	fr=open('Book1.txt','r')
+elif target_file=="Book2":
+        fr=open('Book2.txt','r')
+elif target_file=="Book3":
+        fr=open('Book3.txt','r')
